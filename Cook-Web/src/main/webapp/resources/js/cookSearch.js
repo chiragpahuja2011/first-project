74126ac098ddf5aa4835672f90f6c20f1c71c782
chiragpahuja2011 <chiragpahuja2011@gmail.com>
2016-08-19 20:14:51 +0530

    Added the area filter

--- Cook-Web/src/main/webapp/resources/js/cookSearch.js
+++ Cook-Web/src/main/webapp/resources/js/cookSearch.js
@@ -1,3 +1,25 @@
 function searchCook(){
-window.location.href = "cookSearch.html?area="+"ecity_phase_2";
+
+	var e = document.getElementById("area_search");
+	var areaSelected = e.options[e.selectedIndex].value;
+	if(areaSelected != undefined && "" != areaSelected){
+		window.location.href = "cookSearch.html?area="+areaSelected;
+
+	} else{
+		alert('Please select an area');
+	}
+}
+
+
+function loadScript(){
+	var url = document.location.href,
+	params = url.split('?')[1].split('&'),
+	data = {}, tmp;
+	for (var i = 0, l = params.length; i < l; i++) {
+		tmp = params[i].split('=');
+		data[tmp[0]] = tmp[1];
+	}
+	var element = document.getElementById('area_search');
+	console.log(data.area);
+	element.value = data.area;
 }
