d9fefc5b69bbaaf97c1746b23e7e0f352b8ed33a
chiragpahuja2011 <chiragpahuja2011@gmail.com>
2016-08-22 22:55:31 +0530

    Added a JSON request

--- Cook-Web/src/main/webapp/resources/js/cookSearch.js
+++ Cook-Web/src/main/webapp/resources/js/cookSearch.js
@@ -28,3 +28,28 @@ function signUp(){
 	window.location.href = "signUp"
 }
 
+function submitSignUp(){
+	var user = {};
+	user.fistName = document.getElementById('firstName').value;
+	user.lastName = document.getElementById('lastName').value;
+	user.userName = document.getElementById('userName').value;
+	user.userEmail = document.getElementById('userEmail').value;
+	user.userPassword = document.getElementById('userPassword').value;
+	user.confirmPassword = document.getElementById('confirmPassword').value;
+	user.userAge = document.getElementById('userAge').value;
+
+	var createUser = $.ajax({
+	      type: 'POST',
+	      url: "cook",
+	      data:JSON.stringify(user),
+	      headers: { 
+        		'Accept': 'application/json',
+        		'Content-Type': 'application/json' 
+    	     },
+	      dataType: "json",
+	      success: function(resultData) { alert(" Server call success ful  Save Complete") }
+	});
+	createUser.error(function() { alert("Something went wrong"); });
+	//user.userName = document.ge
+}
+
