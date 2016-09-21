1d6c508e73e3f2a06d361b7c012d970a427a58ae
CHIRAG <chiragpahuja2011@gmail.com>
2016-09-21 16:48:24 +0530

    Added the Server Chaanges

--- Cook-Web/src/main/java/com/community/cook/service/impl/SearchServiceImpl.java
+++ Cook-Web/src/main/java/com/community/cook/service/impl/SearchServiceImpl.java
@@ -1,8 +1,10 @@
 package com.community.cook.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.dozer.DozerBeanMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.community.cook.bean.CookSearchRequest;
+import com.community.cook.bean.UserInformationResponse;
 import com.community.cook.dao.SearchDao;
 import com.community.cook.domain.CookUser;
 import com.community.cook.service.SearchService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
@@ -23,6 +27,12 @@ public class SearchServiceImpl implements SearchService {
 	
 	@Autowired
 	private SearchDao searchDao;
+	
+	@Autowired
+	private ObjectMapper jacksonMapper;
+	
+	@Autowired
+	private DozerBeanMapper mapper;
 
 	/*
 	 * (non-Javadoc)
@@ -31,11 +41,24 @@ public class SearchServiceImpl implements SearchService {
 	@Override
 	@Transactional(readOnly = true)
 	public ObjectNode fetchCookResults(CookSearchRequest cookSearchRequest) {
+		ObjectNode responseNode = jacksonMapper.createObjectNode();
 		List<CookUser> cookUsers = searchDao.getCookResults(cookSearchRequest.getAreas());
 		LOGGER.info("Database Query Finishs");
 		if(CollectionUtils.isNotEmpty(cookUsers)){
-			LOGGER.info("Database Query Finishs"+cookUsers.size());
-
+			LOGGER.info("Database Query Finished"+cookUsers.size());
+			List<UserInformationResponse> userResponse = new ArrayList<UserInformationResponse>();
+			// Iterate Over Cook User Response and build the final Response
+			for(CookUser cookUser: cookUsers){
+				UserInformationResponse userInfoResponse = mapper.map(cookUser, UserInformationResponse.class);
+				
+				if(CollectionUtils.isNotEmpty(cookUser.getCookUserSpecialities())){
+					
+				}
+				
+				userResponse.add(userInfoResponse);
+			}
+			
+			
 		}
 		return null;
 	}
